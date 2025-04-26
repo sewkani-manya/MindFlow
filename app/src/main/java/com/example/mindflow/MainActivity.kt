efb291d0e6bacdd4c7f10f8d543c5e31050d5872
Manya <sewkani.manya564@gmail.com>
2025-04-27 00:06:33 +0530

    Styled UI

--- app/src/main/java/com/example/mindflow/MainActivity.kt
+++ app/src/main/java/com/example/mindflow/MainActivity.kt
@@ -4,14 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mindflow.ui.theme.ButtonBlue
+import com.example.mindflow.ui.theme.LightBackground
 import com.example.mindflow.ui.theme.MindFlowTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +36,57 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MindFlowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
+            }
+        }
+    }
+
+@Composable
+fun MindFlowScreen() {
+    var text by remember { mutableStateOf("") }
+    Surface(color = LightBackground, modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.padding(bottom = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "MindFlow",
+                style = MaterialTheme.typography.headlineLarge
+            )
+            Text(
+                text = "5:00",
+                style = MaterialTheme.typography.displaySmall,
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                placeholder = { Text("Start writing...") },
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(bottom = 24.dp)
+            )
+
+            ElevatedButton(
+                onClick = { /* TODO */ },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = ButtonBlue),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Text("Start Writing")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MindFlowTheme {
-        Greeting("Android")
-    }
+fun MindFlowScreenPreview(){
+    MindFlowScreen()
 }
--- app/src/main/java/com/example/mindflow/ui/theme/Color.kt
+++ app/src/main/java/com/example/mindflow/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightBackground = Color(0xFFFAFAFA)
+val ButtonBlue = Color(0xFF4F8EF7)
